4b04a2c18dc3a7def1485f662a498677ad5d9a59
Christopher Stull <chris.stull@gmail.com>
2018-08-14 19:45:05 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,44 +2,26 @@
 
 katz_deli = []
 
-def line
+def line(katz_deli)
+  order = "The line is currently"
   count = 1
   katz_deli.each do |customer|
-    puts "#{customer}, you are number #{count} in line."
+    order = order + " #{count}. #{customer}"
     count = count + 1
   end
+  puts order
 end
 
-def take_a_number(line,customer)
-  line.push(customer)
-  puts "#{customer}, you are number #{line.size} in line."
+def take_a_number(katz_deli,customer)
+  katz_deli.push(customer)
+  puts "#{customer}, you are number #{katz_deli.size} in line."
 end
 
-def now_serving(line)
-  if line.size > 0
-    puts "Now serving #{line[0]}!"
+def now_serving(katz_deli)
+  if katz_deli.size > 0
+    puts "Currently serving #{katz_deli[0]}!"
+    katz_deli.unshift()
   else
     puts "There is nobody waiting to be served!"
-
-function nowServing(katzDeliLine){
-  if (katzDeliLine.length > 0){
-    var current = katzDeliLine.shift()
-    return `Currently serving ${current}.`
-  }
-  else
-    return "There is nobody waiting to be served!"
-}
-
-function currentLine(katzDeliLine){
-  var theline = "The line is currently: "
-  if (katzDeliLine.length > 0){
-    theline = theline+`1. ${katzDeliLine[0]}`
-    for(let i = 1; i < katzDeliLine.length; i++){
-      theline = theline+`, ${i+1}. ${katzDeliLine[i]}`
-    }
-    return theline
-  }
-  else{
-    return "The line is currently empty."
-  }
-}
+  end
+end
